e36a474569fe540e4b370ef9900f1abd00f0ddb7
WGoodsonDev <warren.goodson1@gmail.com>
2021-07-14 16:08:56 -0700

    Activities CRUD complete

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -16,7 +16,7 @@ namespace API.Controllers
             return await Mediator.Send(new List.Query());
         }
 
-        [HttpGet("{id}")] // activities/id
+        [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query{Id = id});
@@ -33,6 +33,12 @@ namespace API.Controllers
         {
             activity.Id = id;
             return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
     }
 }
