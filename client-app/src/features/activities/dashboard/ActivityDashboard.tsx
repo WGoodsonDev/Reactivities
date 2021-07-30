78088f615e1fb003338c34307c8f205624737a5c
WGoodsonDev <warren.goodson1@gmail.com>
2021-07-30 16:55:16 -0700

    Dashboard/Details implemented

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid, List } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityList from './ActivityList';
 
 interface Props {
     activities: Activity[];
@@ -10,13 +12,11 @@ export default function ActivityDashboard({activities}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <List>
-                    {activities.map((activity) => (
-                        <List.Item key={activity.id}>
-                            {activity.title}
-                        </List.Item>
-                    ))}
-                </List>
+                <ActivityList activities={activities}/>
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] && 
+                <ActivityDetails activity={activities[0]} />}
             </Grid.Column>
         </Grid>
     );
