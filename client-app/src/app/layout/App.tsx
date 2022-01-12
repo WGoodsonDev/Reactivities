414e880716884733df5e0f714c67f8d21fd3d6e5
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-11 20:45:58 -0800

    Ready for MobX refactor

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Container} from 'semantic-ui-react';
+import { Container, Button } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponents';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite'
 
 function App() {
+  const {activityStore} = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -78,6 +82,8 @@ function App() {
     <>
       <NavBar openForm={handleFormOpen}/>
       <Container style={{marginTop: '7em'}}>
+        <h2>{activityStore.title}</h2>
+        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} />
         <ActivityDashboard 
           activities={activities} 
           selectedActivity={selectedActivity}
@@ -95,4 +101,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,13 @@
+import { action, makeAutoObservable, makeObservable, observable } from 'mobx';
+
+export default class ActivityStore {
+    title = 'Hello from MobX!';
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    setTitle = () => {
+        this.title = this.title + '!';
+    }
+}
