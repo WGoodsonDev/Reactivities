31d6f5c535293172a7bc95d419f22ecc26e50eef
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-12 13:17:57 -0800

    Initial MobX setup

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,36 @@
-import { action, makeAutoObservable, makeObservable, observable } from 'mobx';
+import { makeAutoObservable } from 'mobx';
+// import { async } from 'q';
+import agent from '../api/agent';
+import { Activity } from '../models/activity';
 
 export default class ActivityStore {
-    title = 'Hello from MobX!';
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this);
     }
 
-    setTitle = () => {
-        this.title = this.title + '!';
+    loadActivities = async () => {
+        this.setLoadingInitial(true);
+        try {
+            const activities = await agent.Activities.list();
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
+                })
+            this.setLoadingInitial(false);
+        } catch(err) {
+            console.log(err);
+            this.setLoadingInitial(false);
+        }
     }
-}
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
+    }
+
+} 
