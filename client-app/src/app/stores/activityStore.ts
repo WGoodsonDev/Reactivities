5cfaf309e6f99eb1c8544ff9024024701c2a82fe
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-14 11:54:14 -0800

    End of Section 9 (styling)

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,8 +14,19 @@ export default class ActivityStore {
         makeAutoObservable(this); // Automatically binds methods to class as Actions
     }
 
+    // Computed function
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]: Activity[]})
+        )
     }
 
     loadActivities = async () => {
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import { useStore } from '../../../app/stores/store';
 import ActivityList from './ActivityList';
 import { observer } from 'mobx-react-lite';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
+import ActivityFilters from './ActivityFilters';
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
@@ -21,7 +22,7 @@ export default observer(function ActivityDashboard() {
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity filters</h2>                
+                <ActivityFilters />             
             </Grid.Column>
         </Grid>
     );
