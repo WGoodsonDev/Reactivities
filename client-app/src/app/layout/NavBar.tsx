dc7c30a94b153d847fa627a94645651d832f91bd
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-14 10:29:08 -0800

    Redirect after form submission

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/store';
-import { observer } from 'mobx-react-lite';
+import { NavLink } from 'react-router-dom';
 
 
-export default observer( function NavBar() {
-
-    const {activityStore} = useStore();
-    const {openForm} = activityStore;
+export default function NavBar() {
 
     return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to={'/'} exact header>
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: "10px"}}/>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name="Activities"/>
+                <Menu.Item as={NavLink} to={'/activities'} name="Activities"/>
                 <Menu.Item>
-                    <Button onClick={() => openForm()} positive content='Create Activity'/>
+                    <Button as={NavLink} to={'/createActivity'} positive content='Create Activity'/>
                 </Menu.Item>
             </Container>
         </Menu>
     );
-})
+}
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 import agent from '../api/agent';
 import { Activity } from '../models/activity';
-import {v4 as uuid} from 'uuid';
 
 export default class ActivityStore {
     // State to store (observables)
@@ -20,11 +19,11 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
+        this.loadingInitial = true;
         try {
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activityRegistry.set(activity.id, activity);
+                this.setActivity(activity);
             })
             this.setLoadingInitial(false);
         } catch(err) {
@@ -33,30 +32,44 @@ export default class ActivityStore {
         }
     }
 
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if(activity) {
+            this.selectedActivity = activity;
+            return activity;
+        } else {
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(() => {
+                    this.selectedActivity = activity;
+                })
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (err) {
+                console.log(err);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
     }
 
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
-    }
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
-        activity.id = uuid();
         try {
             await agent.Activities.create(activity);
             runInAction(() => { // Run in separate action
@@ -97,7 +110,6 @@ export default class ActivityStore {
             await agent.Activities.delete(id);
             runInAction(() => { // Run in separate action
                 this.activityRegistry.delete(id);
-                if(this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             })
         } catch (err) {
