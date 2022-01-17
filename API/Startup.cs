817b2d2697502a2d53f02f165e0933f264602e68
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-17 11:34:45 -0800

    Exception handler middleware

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 
 using API.Extensions;
+using API.Middleware;
 using Application.Activities;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
@@ -33,9 +34,10 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
